bb051a2e6e9c1ed0e68bad638e2af251c851af64
Ninichhh <nichamon.thub@mail.kmutt.ac.th>
2025-02-05 17:01:17 +0700

    Add files via upload

---
+++ src/Componant/BookingLocation.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const BookingLocation = () => {
+  return (
+    <>
+    <div className='flex justify-center w-60 h-12 bg-blue-900 rounded-[13.88px] text-2xl  text-amber-50 ml-30 mt-10 items-center'>BookingLocation</div>
+    <div className='flex gap-1 ml-30 mt-5 '>
+    <div className='w-100 h-50 bg-white rounded-[13.88px] '></div>
+    <div className='w-100 h-50 bg-white rounded-[13.88px] '></div>
+    <div className='w-100 h-50 bg-white rounded-[13.88px] '></div>
+    </div>
+    </>
+  )
+}
+export default BookingLocation;
